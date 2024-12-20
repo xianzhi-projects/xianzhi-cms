65dd3a3bfb5cf7f27cffca336ac9878ef2e8e00d
Max <max@xianzhi.io>
2024-12-20 18:44:01 +0800

    UI

---
+++ cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/controller/ResourceController.java
@@ -0,0 +1,17 @@
+package io.xianzhi.cms.bootstrap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 资源接口
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/resource")
+public class ResourceController {
+}

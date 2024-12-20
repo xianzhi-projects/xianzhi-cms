e1d226426bed11662fd44637581cae1cad11392e
Max <max@xianzhi.io>
2024-12-20 19:19:55 +0800

    UI

---
+++ cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/business/ColumnBusiness.java
@@ -0,0 +1,10 @@
+package io.xianzhi.cms.bootstrap.business;
+
+/**
+ * 栏目业务类
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+public class ColumnBusiness {
+}
---
+++ cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/business/RoleBusiness.java
@@ -0,0 +1,10 @@
+package io.xianzhi.cms.bootstrap.business;
+
+/**
+ * 角色业务类
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+public class RoleBusiness {
+}

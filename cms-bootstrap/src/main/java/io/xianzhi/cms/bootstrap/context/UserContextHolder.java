ca83cc726793d7dd2d4196e75308d09fc822bdf0
Max <max@xianzhi.io>
2024-12-20 11:33:55 +0800

    添加工具类

---
+++ cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/context/UserContextHolder.java
@@ -0,0 +1,10 @@
+package io.xianzhi.cms.bootstrap.context;
+
+/**
+ * 用户信息上下文
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+public class UserContextHolder {
+}
---
+++ cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/utils/SiteUtils.java
@@ -0,0 +1,23 @@
+package io.xianzhi.cms.bootstrap.utils;
+
+/**
+ * 站点工具类
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+public class SiteUtils {
+
+    /**
+     * 私有化构造器
+     */
+    private SiteUtils() {
+    }
+
+    /**
+     * 站点ID
+     */
+    public static final String REQUEST_HEADER_SITE_ID = "X-Site-Id";
+
+
+}

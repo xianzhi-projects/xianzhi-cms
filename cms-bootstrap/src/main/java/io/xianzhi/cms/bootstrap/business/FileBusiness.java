22e3b8e4765fbecfdd4f2fba5c681ffdd31801b0
Max <max@xianzhi.io>
2024-12-20 19:22:21 +0800

    UI

--- cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/business/FileBusiness.java
+++ cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/business/FileBusiness.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
- * 栏目业务类
+ * 文件业务类
  *
  * @author Max
  * @since 1.0.0
@@ -13,5 +13,5 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ColumnBusiness {
+public class FileBusiness {
 }
